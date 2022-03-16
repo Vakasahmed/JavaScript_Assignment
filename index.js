a4ca04f60a6b9a650e2386ce0cd87e28f78a7ddd
Vakasahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-03-17 00:07:18 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+//Create function for Addition, Subtraction, Multiplication and Division
+//Addition
+function sum(num1,num2){
+    let sumOfResult = num1 + num2
+    return (sumOfResult)
+}
+let result =  sum(5,4);
+console.log(result)
+//Subtraction
+function subtract(num1,num2){
+    let subtractOfResult = num1 - num2
+    return (subtractOfResult)
+}
+let result1 = subtract(99,9);
+console.log(result1)
+//Multiplication
+function mult(num1,num2){
+    let multiplicationOfResult = num1 * num2
+    return (multiplicationOfResult)
+}
+let result3 = mult(22,11);
+console.log(result3)
+//Division
+function div(num1,num2){
+    let divOfResult = num1/num2
+    return (divOfResult)
+}
+let result4 = div(100,10);
+console.log(result4)
+//(a+b)^2
+function formula(a,b){
+    let expansionOfResult = a*a + b*b + (2*a*b)
+    return (expansionOfResult)
+}
+let result5 = formula(2,3);
+console.log(result5)
